7c2aa063981057cc1fdac156dbbf4f46091db354
Boris Lanovenko <blanovenko@gmail.com>
2023-08-02 12:44:42 +0300

    Added name

--- Project1.java
+++ Project1.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Project1{
     public static void main(String[] args) {
         System.out.println("Hi!!!");
+        System.out.print("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name.toUpperCase());
     }
 }
