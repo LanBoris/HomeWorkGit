aa2dba19b51386e620efdc78428e37afe447d698
Boris Lanovenko <blanovenko@gmail.com>
2023-08-02 12:52:32 +0300

    Added hello  with if

--- Project1.java
+++ Project1.java
@@ -6,10 +6,14 @@ public class Project1{
         System.out.print("Enter your name: ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        System.out.println("Hello, " + name.toUpperCase());
         System.out.print("Enter your age: ");
         int age = Integer.parseInt(scanner.nextLine());
         System.out.println("Enter your birth country: ");
         String bcountry = scanner.nextLine();
+        if (age < 13){
+            System.out.println("Hello, little " + name.toUpperCase() + " from " + bcountry.toUpperCase());
+        } else if (age >= 13 && age <20){
+            System.out.println("Hello young " + name.toUpperCase() + " from " + bcountry.toUpperCase());
+        }
     }
 }
